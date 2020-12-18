e28b636e1eb13b209b82ecd085dd11ea45d9a766
Adam Dickmeiss <adam@indexdata.dk>
2020-12-18 14:32:08 +0100

    Importing

---
+++ src/main/java/org/folio/copycat/RecordImporter.java
@@ -0,0 +1,151 @@
+package org.folio.copycat;
+
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpRequest;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
+
+/**
+ * Responsible for importing records.
+ *
+ * Uses mod-record-source-manager for importing.
+ *
+ * https://github.com/folio-org/mod-source-record-manager
+ * https://github.com/folio-org/mod-source-record-manager/blob/master/descriptors/ModuleDescriptor-template.json
+ * https://github.com/folio-org/mod-source-record-manager/blob/master/ramls/change-manager.raml
+ * https://github.com/folio-org/data-import-raml-storage/blob/master/schemas/dto/initJobExecutionsRqDto.json
+ * https://github.com/folio-org/mod-source-record-manager/blob/master/README.md#data-import-workflow
+ */
+public class RecordImporter {
+
+  private static Logger log = LogManager.getLogger(RecordImporter.class);
+  private final WebClient client;
+  private final Map<String, String> okapiHeaders;
+  private final String okapiUrl;
+  private final String userId;
+  private String jobId;
+
+  /**
+   * Constructor for importing (can NOT be shared between users/tenants).
+   *
+   * @param okapiHeaders essential headers for importing
+   * @param context Vert.x context
+   */
+  public RecordImporter(Map<String, String> okapiHeaders, Context context) {
+    client = WebClient.create(context.owner());
+    this.okapiUrl = okapiHeaders.get("X-Okapi-Url");
+    this.userId = okapiHeaders.get("X-Okapi-User-Id");
+    this.okapiHeaders = okapiHeaders;
+  }
+
+  Future<String> createJob() {
+    String abs = okapiUrl + "/change-manager/jobExecutions";
+    HttpRequest<Buffer> request = client.postAbs(abs);
+    request.headers().addAll(okapiHeaders);
+    JsonObject jobProfileInfo = new JsonObject();
+    jobProfileInfo.put("id", "c8f98545-898c-4f48-a494-3ab6736a3243");
+    jobProfileInfo.put("name", "Default job profile");
+    jobProfileInfo.put("dataType", "MARC");
+
+    JsonObject initJob = new JsonObject();
+    initJob.put("userId", userId);
+    initJob.put("sourceType", "ONLINE");
+    initJob.put("jobProfileInfo", jobProfileInfo);
+    log.info("createJob with {}", initJob.encode());
+    // With Vert.x 4 this may be simpler
+    Promise<String> promise = Promise.promise();
+    request.sendJsonObject(initJob, res -> {
+      if (res.failed()) {
+        log.error(res.cause().getMessage(), res.cause());
+        promise.fail(res.cause());
+        return;
+      }
+      HttpResponse<Buffer> result = res.result();
+      if (result.statusCode() != 201) {
+        log.error("{} returned {}", abs, result.statusCode());
+        promise.fail(abs + " returned " + result.statusCode() + " (expected 201):" + result.bodyAsString());
+        return;
+      }
+      promise.complete(result.bodyAsJsonObject().getString("parentJobExecutionId"));
+    });
+    return promise.future();
+  }
+
+  /**
+   * begin importing for current user/tenant.
+   *
+   * @return async result.
+   */
+  public Future<Void> begin() {
+    return createJob().compose(id -> {
+      jobId = id;
+      return Future.succeededFuture();
+    });
+  }
+
+  Future<Void> post(JsonObject record, boolean last) {
+    String abs = okapiUrl + "/change-manager/jobExecutions/" + jobId + "/records";
+    HttpRequest<Buffer> request = client.postAbs(abs);
+    request.headers().addAll(okapiHeaders);
+
+    JsonObject recordsMetadata = new JsonObject();
+    recordsMetadata.put("last", last);
+    recordsMetadata.put("contentType", "MARC_JSON");
+    recordsMetadata.put("counter", 1);
+    recordsMetadata.put("total", 1);
+
+    JsonArray initialRecords = new JsonArray();
+    if (record != null) {
+      initialRecords.add(new JsonObject().put("record", record.encodePrettily()));
+    }
+    JsonObject rawRecordsDto = new JsonObject();
+    rawRecordsDto.put("recordsMetadata", recordsMetadata);
+    rawRecordsDto.put("initialRecords", initialRecords);
+    // With Vert.x 4 this may be simpler
+    Promise<Void> promise = Promise.promise();
+    request.sendJsonObject(rawRecordsDto, res -> {
+      if (res.failed()) {
+        log.error(res.cause().getMessage(), res.cause());
+        promise.fail(res.cause());
+        return;
+      }
+      HttpResponse<Buffer> result = res.result();
+      if (result.statusCode() != 204) {
+        log.error("{} returned {}", abs, result.statusCode());
+        promise.fail(abs  + " returned " + result.statusCode() + " (expected 204):" + result.bodyAsString());
+        return;
+      }
+      promise.complete();
+    });
+    return promise.future();
+  }
+
+
+  /**
+   * post record for importing.
+   *
+   * @param record record to be imported
+   * @return async result.
+   */
+  public Future<Void> post(JsonObject record) {
+    return post(record, false);
+  }
+
+  /**
+   * end importing.
+   *
+   * @return async result.
+   */
+  public Future<Void> end() {
+    return post(null, true).onComplete(x -> client.close());
+  }
+}
---
+++ src/test/java/org/folio/rest/impl/ImporterMock.java
@@ -0,0 +1,117 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.UUID;
+
+public class ImporterMock {
+  private final Vertx vertx;
+  private HttpServer server;
+  private static final Logger log = LogManager.getLogger(CopycatAPI.class);
+  private int createStatus = 201;
+  private int importStatus = 204;
+
+  Set<String> jobs = new TreeSet<>();
+
+  public ImporterMock(Vertx vertx) {
+    this.vertx = vertx;
+  }
+
+  public void setCreateStatus(int code) {
+    createStatus = code;
+  }
+
+  public void setCImportStatus(int code) {
+    importStatus = code;
+  }
+
+  public void createJob(RoutingContext ctx) {
+    try {
+      JsonObject requestBody = ctx.getBodyAsJson();
+      String userId = requestBody.getString("userId");
+      UUID uuid = UUID.fromString(userId);
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+      ctx.response().setStatusCode(500);
+      ctx.response().end(e.getMessage());
+      return;
+    }
+    JsonObject responseBody = new JsonObject();
+    String id = UUID.randomUUID().toString();
+
+    jobs.add(id);
+    responseBody.put("parentJobExecutionId", id);
+
+    ctx.response().putHeader("Content-Type", "application/json");
+    ctx.response().setStatusCode(createStatus);
+    ctx.response().end(responseBody.encode());
+  }
+
+  public void importJob(RoutingContext ctx) {
+    try {
+      // /change-manager/jobExecutions/{jobId}/records
+      String path = ctx.request().path();
+      String [] comp = path.split("/");
+      String id = comp[3];
+      if (!jobs.contains(id)) {
+        ctx.response().setStatusCode(404);
+        ctx.response().end("Job not found " + id);
+        return;
+      }
+      JsonObject requestBody = ctx.getBodyAsJson();
+      if (!requestBody.containsKey("initialRecords")) {
+        ctx.response().setStatusCode(400);
+        ctx.response().end("Missing initialRecords");
+        return;
+      }
+      JsonObject recordsMetadata = requestBody.getJsonObject("recordsMetadata");
+      if (Boolean.TRUE.equals(recordsMetadata.getBoolean("last"))) {
+        jobs.remove(id);
+      }
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+      ctx.response().setStatusCode(500);
+      ctx.response().end(e.getMessage());
+      return;
+    }
+    ctx.response().setStatusCode(importStatus);
+    ctx.response().end();
+  }
+
+  public Future<Void> start(int port) {
+    Router router = Router.router(vertx);
+    router.postWithRegex("/change-manager/jobExecutions.*").handler(BodyHandler.create());
+    router.post("/change-manager/jobExecutions").handler(this::createJob);
+    router.postWithRegex("/change-manager/jobExecutions/.*").handler(this::importJob);
+    Promise<Void> promise = Promise.promise();
+    vertx.createHttpServer()
+        .requestHandler(router)
+        .listen(port, x -> {
+              if (x.succeeded()) {
+                server = x.result();
+              }
+              promise.handle(x.mapEmpty());
+            });
+    return promise.future();
+  }
+
+  public Future<Void> stop() {
+    if (server != null) {
+      server.close();
+    }
+    return Future.succeededFuture();
+  }
+}
