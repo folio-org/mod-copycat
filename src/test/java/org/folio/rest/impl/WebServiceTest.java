c0bb4d3b594640b469b52995c5a3f992de71c992
Adam Dickmeiss <adam@indexdata.dk>
2021-03-01 17:11:12 +0100

    Extend with {create,update}JobProfileId MODCPCT-14
    These are *required* for mod-copycat to operate as these are no
    longer part of the code itself. Update the reference data records
    to use these. Add test that uses mod-copycat as web service.

---
+++ src/test/java/org/folio/rest/impl/WebServiceTest.java
@@ -0,0 +1,81 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import java.util.Collections;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.folio.okapi.common.XOkapiHeaders;
+import org.folio.rest.RestVerticle;
+import org.folio.rest.client.TenantClient;
+import org.folio.rest.jaxrs.model.Parameter;
+import org.folio.rest.jaxrs.model.TenantAttributes;
+import org.folio.rest.tools.utils.TenantInit;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(VertxExtension.class)
+class WebServiceTest {
+  private static final Logger log = LogManager.getLogger(CopycatImpl.class);
+
+  private static final String TENANT = "tenant";
+  private static final int mockPort = 9231;
+  private static final int port = 9230;
+  private static ImporterMock mock;
+  private static WebClient webClient;
+
+  @BeforeAll
+  static void beforeAll(Vertx vertx, VertxTestContext context) {
+    DeploymentOptions options = new DeploymentOptions().setConfig(
+      new JsonObject()
+        .put("http.port", port));
+
+    webClient = WebClient.create(vertx);
+    mock = new ImporterMock(vertx);
+    vertx.deployVerticle(RestVerticle.class.getName(), options)
+      .compose(a -> mock.start(mockPort))
+      .onComplete(context.succeeding(x -> {
+        log.debug("beforeAll completed");
+        context.completeNow();
+      }));
+  }
+
+  @AfterAll
+  static void afterAll(Vertx vertx, VertxTestContext context) {
+    webClient.close();
+    vertx.close(context.succeeding(x -> {
+      log.debug("afterAll completed");
+      context.completeNow();
+    }));
+  }
+
+  @Test
+  void testReferenceDataLoaded(Vertx vertx, VertxTestContext context) {
+    TenantClient tenantClient = new TenantClient("http://localhost:" + port, TENANT, null);
+    TenantAttributes tenantAttributes = new TenantAttributes()
+      .withModuleTo("mod-copycat-1.0.0")
+      .withParameters(Collections.singletonList(new Parameter().withKey("loadReference").withValue("true")));
+
+    TenantInit.exec(tenantClient, tenantAttributes, 60000)
+      .compose(res -> webClient.get(port, "localhost", "/copycat/profiles")
+        .putHeader(XOkapiHeaders.TENANT, TENANT)
+        .send())
+      .compose(res -> {
+        JsonObject bodyResponse = res.bodyAsJsonObject();
+        assertThat(bodyResponse.getInteger("totalRecords")).isEqualTo(2); // number of reference records
+        return Future.succeededFuture();
+      })
+      .compose(res -> TenantInit.purge(tenantClient, 60000))
+      .onComplete(context.succeeding(x -> context.completeNow())
+      );
+  }
+}
