3e9c29219f3a3436e8f211cb3f9b6ef58944eb30
Adam Dickmeiss <adam@indexdata.dk>
2021-05-12 13:22:04 +0200

    Test module upgrade with reference data
    Test that "authentication" can be set and that upgrade with
    reference data does not tamper with that.

--- src/test/java/org/folio/rest/impl/WebServiceTest.java
+++ src/test/java/org/folio/rest/impl/WebServiceTest.java
@@ -29,6 +29,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class WebServiceTest {
   private static final Logger log = LogManager.getLogger(CopycatImpl.class);
 
+  private static final String OCLC_WORLDCAT_ID = "f26df83c-aa25-40b6-876e-96852c3d4fd4";
   private static final String TENANT = "tenant";
   private static final int mockPort = 9231;
   private static final int port = 9230;
@@ -74,8 +75,36 @@ class WebServiceTest {
         .putHeader(XOkapiHeaders.TENANT, TENANT)
         .send())
       .compose(res -> {
+        assertThat(res.statusCode()).isEqualTo(200);
         JsonObject bodyResponse = res.bodyAsJsonObject();
         assertThat(bodyResponse.getInteger("totalRecords")).isEqualTo(2); // number of reference records
+        return Future.succeededFuture();
+      })
+      .compose(res ->
+        webClient.get(port, "localhost", "/copycat/profiles/" + OCLC_WORLDCAT_ID)
+          .putHeader(XOkapiHeaders.TENANT, TENANT)
+          .send())
+      .compose(res -> {
+        assertThat(res.statusCode()).isEqualTo(200);
+        JsonObject oclcProfile = res.bodyAsJsonObject().put("authentication", "foo/bar");
+        return webClient.put(port, "localhost", "/copycat/profiles/" + OCLC_WORLDCAT_ID)
+          .putHeader(XOkapiHeaders.TENANT, TENANT)
+          .putHeader("Content-Type", "application/json")
+          .sendJson(oclcProfile);
+      })
+      .compose(res -> {
+        assertThat(res.statusCode()).isEqualTo(204);
+        tenantAttributes.setModuleFrom("mod-copycat-1.0.0");
+        tenantAttributes.setModuleTo("mod-copycat-1.0.1");
+        return TenantInit.exec(tenantClient, tenantAttributes, 60000);
+      })
+      .compose(res ->
+        webClient.get(port, "localhost", "/copycat/profiles/" + OCLC_WORLDCAT_ID)
+          .putHeader(XOkapiHeaders.TENANT, TENANT)
+          .send())
+      .compose(res -> {
+        assertThat(res.statusCode()).isEqualTo(200);
+        assertThat(res.bodyAsJsonObject().getString("authentication")).isEqualTo("foo/bar");
         return Future.succeededFuture();
       })
       .compose(res -> TenantInit.purge(tenantClient, 60000))
