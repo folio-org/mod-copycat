24c302fec01cefd477162f70178eb0825ae8d998
Kyrylo_Shulzhenko <Kyrylo_Shulzhenko@epam.com>
2022-12-09 10:19:28 +0200

    [MODCPCT-69]Create an upgrade process for existing Z39.50 target profiles

---
+++ src/main/resources/templates/db_scripts/update_profile.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.update_cpct_profile(
+	input_jsonb jsonb)
+    RETURNS jsonb
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+begin
+  IF(input_jsonb::jsonb->'allowedCreateJobProfileIds' IS NOT NULL) THEN
+	  input_jsonb = jsonb_set(input_jsonb,'{allowedCreateJobProfileIds}',jsonb_build_array(input_jsonb::jsonb->'createJobProfileId'));
+	END IF;
+	IF(input_jsonb::jsonb->'allowedUpdateJobProfileIds' IS NOT NULL) THEN
+  	input_jsonb = jsonb_set(input_jsonb,'{allowedUpdateJobProfileIds}',input_jsonb::jsonb->'updateJobProfileId');
+  END IF;
+
+	return input_jsonb;
+end
+$BODY$;
+
+UPDATE ${myuniversity}_${mymodule}.profile
+  SET jsonb=${myuniversity}_${mymodule}.update_cpct_profile(jsonb);
+
+DROP FUNCTION IF EXISTS ${myuniversity}_${mymodule}.update_cpct_profile(input_jsonb jsonb);
