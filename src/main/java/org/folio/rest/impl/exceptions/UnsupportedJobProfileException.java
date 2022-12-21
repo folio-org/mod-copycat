1bdfc8785c6a1e8e8179a4734c98914a40ec526f
RomanChernetskyi <111740564+RomanChernetskyi@users.noreply.github.com>
2022-12-21 16:41:06 +0200

    Allow importing single record with selected jobProfile (#102)
    * [MODCPCT-71] Allow importing single records with selected jobProfile
    
    * [MODCPCT-69]Create an upgrade process for existing Z39.50 target profiles

---
+++ src/main/java/org/folio/rest/impl/exceptions/UnsupportedJobProfileException.java
@@ -0,0 +1,7 @@
+package org.folio.rest.impl.exceptions;
+
+public class UnsupportedJobProfileException extends Exception {
+  public UnsupportedJobProfileException(String message) {
+    super(message);
+  }
+}
